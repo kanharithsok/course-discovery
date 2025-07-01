c798ce59442f1f283059213ef0113aba023cf071
Kanharith Sok <kanharithsok@gmail.com>
2025-07-01 22:48:02 +0700

    Update logging

--- course_discovery/apps/api/v1/views/search.py
+++ course_discovery/apps/api/v1/views/search.py
@@ -105,7 +105,10 @@ class CourseSearchViewSet(BaseElasticsearchDocumentViewSet):
     def get_queryset(self):
         queryset = super().get_queryset()
         queryset = queryset.filter('term', **{'title.raw': 'Internal 1'})
-        print('DEBUG ES QUERY:', queryset.to_dict(), flush=True)
+        # print('DEBUG ES QUERY:', queryset.to_dict(), flush=True)
+        import logging
+        logger = logging.getLogger(__name__)
+        logger.warning('DEBUG ES QUERY: %s', queryset.to_dict())
         return queryset
 
 
